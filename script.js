9491ebd81418459efcf660b6554fc1a998abd0a5
EzeCrazy <123467580+EzeCrazy@users.noreply.github.com>
2026-04-27 11:40:20 +0300

    (Homework №4): JS introduce -rewriting the code from the lecture

---
+++ script.js
@@ -0,0 +1,30 @@
+const recolorFirstCardBtn = document.querySelector('#recolor-first-card-btn');
+const firstProductCard = document.querySelector('.product-card');
+const blueHashColor = '#0000FF';
+
+recolorFirstCardBtn.addEventListener('click', () => {
+    firstProductCard.style.backgroundColor = blueHashColor;
+    console.log(allProductCards)
+})
+
+const greenHashColor = '#00FF44'
+
+const allProductCards = document.querySelectorAll('.product-card');
+const recolorAllCardsBtn = document.getElementById('recolor-all-cards-btn');
+
+recolorAllCardsBtn.addEventListener('click', () => {
+    allProductCards.forEach(
+        card => card.style.backgroundColor = greenHashColor
+    )
+})
+
+const googleTransitionButton = document.getElementById('go-google-btn');
+googleTransitionButton.addEventListener('click', openGoogle)
+
+function openGoogle() {
+    const answer = confirm('Вы действительно хотите перейти на сайт Google?')
+
+    if (answer === true) {
+        window.open('https://google.com');
+    }
+}
