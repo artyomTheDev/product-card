7e7318e63f1d375ab136ee5fc049cd9e0625ba4c
EzeCrazy <123467580+EzeCrazy@users.noreply.github.com>
2026-05-18 11:43:16 +0300

    (Homework №6): JS tasks -adding two objects

---
+++ homework-6.js
@@ -0,0 +1,20 @@
+const infoAboutMe = {
+    name: 'Артём',
+    surname: 'Лебедев',
+    email: '',
+    work: '',
+    age: 23,
+    country: '',
+    martialStatus: '',
+    language: ['русский',' французский', ' английский'],
+    education: '',
+    hobbies: [' спорт', ' программирование']
+}
+
+const dreamCar = {
+    mark: '',
+    model: '',
+    color: '',
+    vehicleYear: '',
+    transmissionsType: '',
+}
